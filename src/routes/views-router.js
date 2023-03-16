97c68346b671a5afb9fa7129aead069263e9fa5b
Tino <valentinoaraya04@gmail.com>
2023-03-15 23:39:02 -0300

    Cambio fondo

--- src/routes/views-router.js
+++ src/routes/views-router.js
@@ -11,10 +11,7 @@ viewsRouter.get("/", async (req,res)=>{
 
 viewsRouter.get("/real-time-products", async (req,res)=>{
     const products = await manager.getProducts()
-    res.render("real-time-products", {products})            
-
-   // opción 2 emitir los products por websocket
-    
+    res.render("real-time-products", {products})             
 })
 
 export default viewsRouter
